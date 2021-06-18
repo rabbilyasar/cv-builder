0795c3a1eca8981541fd0626524678f5f83a8bcf
Juan Rios <juankrios15@gmail.com>
2021-06-18 18:56:16 +0000

    auth classes

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -102,10 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
 
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ]
 
 }
 
